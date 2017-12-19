49f6b77b7fd2e39508bd5483ebed0a06d9ab8a1e
Insomnius <aliana.arib@gmail.com>
2017-12-19 16:32:42 +0700

    Menyelesaikan range.go

--- range.go
+++ range.go
@@ -15,4 +15,22 @@ func main() {
 
 	fmt.Println("jumlah value:", sum)
 
+	for i, num := range nums {
+		if num == 3 {
+			fmt.Println("index:", i)
+		}
+	}
+
+	kvs := map[string]string{"a": "apple", "b": "banana"}
+	for k, v := range kvs {
+		fmt.Printf("%s -> %s\n", k, v)
+	}
+
+	for k := range kvs {
+		fmt.Println("key:", k)
+	}
+
+	for i, c := range "go" {
+		fmt.Println(i, c)
+	}
 }
